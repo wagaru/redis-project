2411c11211b3b5d038e12597fe091b7c6cca72af
wagaru <wagaru@hotmail.com>
2021-11-29 15:46:31 +0800

    add testcase

--- delivery/register_test.go
+++ delivery/register_test.go
@@ -25,10 +25,30 @@ func TestRegisterUser(t *testing.T) {
 
 	mux.ServeHTTP(w, req)
 
-	responseMap := make(map[string]interface{}, 0)
+	responseMap := make(map[string]interface{})
 	json.Unmarshal(w.Body.Bytes(), &responseMap)
 
 	if _, ok := responseMap["token"]; !ok {
 		t.Errorf("Returen not contain token, failed.")
+	}
+}
+
+func TestRegisterUserFailed(t *testing.T) {
+	val := url.Values{}
+	val.Set("name", "")
+	req := httptest.NewRequest("POST", "/register", strings.NewReader(val.Encode()))
+	w := httptest.NewRecorder()
+
+	mux := http.NewServeMux()
+	usecase := NewMockUsecase()
+	delivery := NewHttpDelivery(mux, usecase)
+
+	delivery.buildRoute()
+	mux.ServeHTTP(w, req)
+	responseMap := make(map[string]interface{})
+	json.Unmarshal(w.Body.Bytes(), &responseMap)
+
+	if _, ok := responseMap["err"]; !ok {
+		t.Errorf("Return not contain err, failed.")
 	}
 }
